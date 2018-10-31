49d21ae1f3837fc9468c52be4c4aa8d73cb0cbc7
David Douard <david.douard@sdfa3.org>
2018-10-31 16:54:50 +0100

    cli: add support for cli options --host and --token
    to override config from ~/.arcrc
    Also ensure a dedicated cache directory if used when using one of those
    options.

--- src/pyarcanist/__init__.py
+++ src/pyarcanist/__init__.py
@@ -1,5 +1,9 @@
 from .cache import cache
+from . import cli
+
+# make sure these are imported after cli, since some
+# cache config may occur in there
 from . import whoami
 from . import diff
 
-__all__ = (whoami, diff, cache)
+__all__ = (cli, whoami, diff, cache)
--- src/pyarcanist/cli.py
+++ src/pyarcanist/cli.py
@@ -1,5 +1,10 @@
+import sys
 import click
+import hashlib
+
 from phabricator import Phabricator
+
+from . import cache
 
 # we use a global variable to store the Phabricator instance so we do not
 # have to add the cnx argument to several utility functions which can then
@@ -17,13 +22,29 @@ class options(dict):
 
 @click.group()
 @click.option('-v', '--verbose/--no-verbose', default=False, envvar='VERBOSE')
+@click.option('-h', '--host', default=None, envvar='PHAB_CONDUIT_URL')
+@click.option('-t', '--token', default=None, envvar='PHAB_CONDUIT_TOKEN')
+
 @click.pass_context
-def pyarc(ctx, verbose):
+def pyarc(ctx, verbose, host, token):
     """Entry point"""
     global cnx
     ctx.ensure_object(dict)
-    ctx.obj['cnx'] = cnx = Phabricator()
-    ctx.obj['options'] = options(verbose=verbose)
+    kwargs = {}
+    if host:
+        kwargs['host'] = host
+    if token:
+        kwargs['token'] = token
+    if host or token:
+        hkey = hashlib.sha256('{}:{}'.format(
+            host or '', token or '').encode()).hexdigest()
+        cache.kwargs['data_dir'] += '/{}'.format(hkey)
+    ctx.obj['cnx'] = cnx = Phabricator(**kwargs)
+    ctx.obj['options'] = options(verbose=verbose, host=host, token=token)
+    if verbose:
+        click.echo('Connecting to:')
+        click.echo('  API endpoint: {}'.format(cnx.host))
+        click.echo('  Token: {}'.format(cnx.token))
 
 
 if __name__ == '__main__':
